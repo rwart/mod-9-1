42bc970c2167d54974bc5eb2e57e4da7a4b6856d
rwart <32432397+rwart@users.noreply.github.com>
2017-10-12 11:46:39 +0200

    Added new function and index.html improvements

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,4 @@
-'use strict'
-
+'use strict';
 function getTriangleArea(a, h) {
   if ((a <= 0) || (h <= 0)) {
     return 'Nieprawidłowe dane';
@@ -8,7 +7,11 @@ function getTriangleArea(a, h) {
   }
 }
 
-console.log(getTriangleArea(10, 6));
+function getTriangleAreaShort(a, h) {
+  return ((a > 0) && (h > 0)) ? a * h / 2 : 'Nieprawidłowe dane';
+}
+
+console.log('TriangleArea = ' + getTriangleArea(10, 6));
 
 var triangle1Area = getTriangleArea(10, 15);
 var triangle2Area = getTriangleArea(20, 20);
@@ -17,3 +20,9 @@ var triangle3Area = getTriangleArea(30, 25);
 console.log('triangle1Area = ' + triangle1Area +
           '  triangle2Area = ' + triangle2Area +
           '  triangle3Area = ' + triangle3Area);
+
+console.log('Short:');
+console.log('TriangleArea = ' + getTriangleAreaShort(10, 6));
+console.log('triangle1Area = ' + getTriangleAreaShort(10, 15) +
+          '  triangle2Area = ' + getTriangleAreaShort(20, 20) +
+          '  triangle3Area = ' + getTriangleAreaShort(30, 25));
